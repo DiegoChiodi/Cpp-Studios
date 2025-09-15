fbb07d9976419a78b457ed115a11128577f59bd1
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-09-15 20:13:43 -0300

    path-body create

---
+++ Testing-cpp-limits/path-body.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main (int argc, char* argv[])
+{
+    int opcao = 0;
+    while (opcao != 5)
+    {
+        std::cout << "Escolha uma opção" << std::endl;
+        
+        
+        std::cout << "5 - Sair" << std::endl;
+        std::cin >> opcao;
+    
+        switch (opcao)
+        {
+        }
+         
+    }
+    
+}
