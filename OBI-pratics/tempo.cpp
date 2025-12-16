b167f3e869f6b3deee44cc84429fd846696f0133
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-12-16 18:23:55 -0300

    fix: resolution OBI, 80/100 -> 100/100

--- OBI-pratics/tempo.cpp
+++ OBI-pratics/tempo.cpp
@@ -2,100 +2,118 @@
 
 using namespace std;
 
-int reg;
 
-int tempResp(int ind, char stats[], int valores[], const int &tam)
+//  Ordena dois vetores em paralelo pelo friend_i
+
+void bubbleSort(int friend_i[], int resp[], int n)
 {
-    bool prev_is_t = false;
-    int tempTot = 0;
-
-    for (int i = ind + 1; i < tam; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        switch(stats[i])
+        for (int j = 0; j < n - i - 1; j++)
         {
-            case 'R':
-                if (!prev_is_t)
-                {
-                    tempTot += 1;
-                }
-                
-                prev_is_t = false;
-                break;
-            case 'E':
-                if (!prev_is_t)
-                {
-                    tempTot += 1;
-                }
-
-                if (valores[i] == valores[ind])
-                {
-                    return tempTot;
-                }
-                prev_is_t = false;
-                break;
-            case 'T':
-                prev_is_t = true;
-                tempTot += valores[i];
-                break;
+            if (friend_i[j] > friend_i[j + 1])
+            {
+                swap(friend_i[j], friend_i[j + 1]);
+                swap(resp[j], resp[j + 1]);
+            }
         }
     }
-    return -1;
 }
 
 
-int main (void)
+//  Calcula a resposta temporária a partir do índice ind
+
+int tempResp(int ind, char stats[], int valores[], int n)
 {
+    bool acumulandoTempo = false;
+    int total = 0;
+
+    for (int i = ind + 1; i < n; i++)
+    {
+        if (stats[i] == 'R')
+        {
+            if (!acumulandoTempo)
+                total++;
+            acumulandoTempo = false;
+        }
+        else if (stats[i] == 'E')
+        {
+            if (!acumulandoTempo)
+                total++;
+
+            if (valores[i] == valores[ind])
+                return total;
+
+            acumulandoTempo = false;
+        }
+        else if (stats[i] == 'T')
+        {
+            acumulandoTempo = true;
+            total += valores[i];
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int reg;
     cin >> reg;
 
     char stats[reg];
     int valores[reg];
 
     for (int i = 0; i < reg; i++)
-    {
         cin >> stats[i] >> valores[i];
-    }
 
-    int friend_i[20] = {};
-    int resp[20] = {};
+    int friend_i[reg];
+    int resp[reg];
+    int qtd = 0;
 
-
+    // Coleta respostas válidas
     for (int i = 0; i < reg; i++)
     {
         if (stats[i] == 'R')
         {
-            friend_i[i] = valores[i];
-            int tmp_resp = tempResp(i, stats, valores, reg);
-            if (tmp_resp == -1)
+            friend_i[qtd] = valores[i];
+            resp[qtd] = tempResp(i, stats, valores, reg);
+            qtd++;
+        }
+    }
+
+    // Agrupa amigos iguais
+    for (int i = 0; i < qtd; i++)
+    {
+        for (int j = i + 1; j < qtd; j++)
+        {
+            if (friend_i[i] == friend_i[j])
             {
-                resp[i] = -1;
-            } else {
-                resp[i] += tmp_resp;
+                if (resp[i] == -1 || resp[j] == -1)
+                {
+                    resp[i] = -1;
+                }
+                else
+                {
+                    resp[i] += resp[j];
+                }
+
+                // remove j
+                friend_i[j] = friend_i[qtd - 1];
+                resp[j]     = resp[qtd - 1];
+                qtd--;
+                j--;
             }
         }
     }
 
-    for (int i = 0; i < 20; i++)
+    // Ordena uma única vez
+    bubbleSort(friend_i, resp, qtd);
+
+    // Imprime
+    for (int i = 0; i < qtd; i++)
     {
-        for(int j = i + 1; j < 20; j++)
-        {
-            if (friend_i[i] == friend_i[j])
-            {
-                if (resp[j] == -1)
-                {
-                    resp[i] = -1;
-                    friend_i[j] = 0;
-                } else 
-                {
-                    resp[i] += resp[j];
-                    friend_i[j] = 0;
-                }
-            }
-        }
-        if (friend_i[i] != 0)
-        {
-            cout << friend_i[i] << ' ' << resp[i] << endl;
-        }
+        cout << friend_i[i] << " " << resp[i] << endl;
     }
-    
     return 0;
-};
+}
