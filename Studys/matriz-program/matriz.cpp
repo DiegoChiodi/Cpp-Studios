ee16c552bcae3cd6b008da6dc14b02789252e3c0
DiegoChiodi <dchiodigoncalves@gmail.com>
2026-04-03 19:50:07 -0300

    feat: matrix multiplication add

--- Studys/matriz-program/matriz.cpp
+++ Studys/matriz-program/matriz.cpp
@@ -12,7 +12,7 @@ vector<vector<int>> fill_matrix(int lines, int columns)
     {
         for (int j = 0; j < columns; j++)
         {
-            cout << "Digite um valor para o elemento localizado em i = " << i << " x j = " << j << '.' << endl;
+            cout << "Digite um valor para o elemento localizado em i = " << i + 1 << " x j = " << j + 1 << '.' << endl;
             cin >> matrix[i][j];
         }
     }
@@ -35,6 +35,7 @@ vector<vector<int>> create_matrix()
 
 void print_matriz(vector<vector<int>> _matrix)
 {
+    cout << "Matriz:" << endl;
     for (int i = 0; i < _matrix.size(); i++)
     {
         for (int j = 0; j < _matrix[i].size(); j++)
@@ -55,6 +56,7 @@ void addition_matrix()
             matrix_main[i][j] += matrix[i][j];
         }
     }
+    cout << "Operação realizada com sucesso" << endl;
 }
 
 void mult_matrix()
@@ -65,49 +67,58 @@ void mult_matrix()
     cout << "Digite o número de colunas da matrix que vai multiplicar: " << endl;
     std::cin >> columns_r;
     vector<vector<int>> matrix_mult = fill_matrix(matrix_main[0].size(), columns_r);
-    vector<vector<int>> matrix_resul(lines_r, vector<int>(columns_r, 0));
+    vector<vector<int>> matrix_res(lines_r, vector<int>(columns_r, 0));
     
     for (int i = 0; i < lines_r; i++)
     {
         for (int j = 0; j < columns_r; j++)
         {
-
+            for (int k = 0; k < lines_r; k++)
+            {
+                matrix_res[i][j] += matrix_main[i][k] * matrix_mult[k][j];
+            }
         }
     }
+    matrix_main = matrix_res;
+    cout << "Operação realizada com sucesso" << endl;
 }
 
 
 void manager()
 {
     int opcao = -1;
+    bool start = false;
     while(opcao != 9)
     {
-        if (matrix_main.empty())
+        if (!start)
         {
             matrix_main = create_matrix();
+            start = true;
         } else {
-            cout << "Escolha uma opcao:" << endl;
+            cout << "Escolha uma opção:" << endl;
             cout << "1 - Substituir matriz atual." << endl;
             cout << "2 - Visualizar matriz atual." << endl;
-            cout << "3 - Adicao + de matrizes" << endl;
-            cout << "4 - Multiplicacao * de matrizes." << endl;
+            cout << "3 - Adição + de matrizes" << endl;
+            cout << "4 - Multiplicação * de matrizes." << endl;
             cout << "5 - Transpor matriz atual." << endl;
-            cout << "6 - Verificar se a matriz atual e simetrica." << endl;
+            cout << "6 - Verificar se a matriz atual é simétrica." << endl;
             cout << "7 - Sair X." << endl;
 
             cin >> opcao;   
-
             switch(opcao)
             {
                 case 1:
                     matrix_main = create_matrix();
+                    break;
                 case 2:
                     print_matriz(matrix_main);
+                    break;
                 case 3:
                     addition_matrix();
+                    break;
                 case 4:
                     mult_matrix();
-
+                    break;
             }
         }
     }
